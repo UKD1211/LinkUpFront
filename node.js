db828c487dab2be033a3cb0c20711392f634744e
Uttam <duttau326@gmail.com>
2022-10-09 20:27:10 +0530

    Initial commit

---
+++ node.js
@@ -0,0 +1,18 @@
+let sidebarActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleActivity() {
+    sidebarActivity.classList.toggle("open-activity");
+
+
+    if (sidebarActivity.classList.contains("open-activity")) {
+        moreLink.innerHTML = "Show less <b>-</b>";
+    }
+    else {
+        moreLink.innerHTML = "Show more <b>+</b>";
+    }
+
+
+
+
+}
